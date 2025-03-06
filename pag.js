e67e868bdcab5d6d353b3b46c5c2ad519804beeb
Wellington-Silva-create <wellingtonlacerda2626@gmail.com>
2025-03-06 10:45:46 -0300

    Update pag.js

--- pag.js
+++ pag.js
@@ -1,1 +1,7 @@
-console.log("Parabens...")
+fetch("https://sua-url-aqui.com", { 
+    method: "POST",
+    body: JSON.stringify({ teste: "dados" }),
+    headers: { "Content-Type": "application/json" }
+})
+.then(res => console.log(res))
+.catch(err => console.error(err));
